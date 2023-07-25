51d91becb4edaca81e68e7c4248babefe2c3308f
unknown <medamineouslati85@gmail.com>
2023-07-25 18:37:16 +0200

    like

---
+++ Week3/Day 1/Core/likes.js
@@ -0,0 +1,6 @@
+var a = document.querySelector(".b")
+var x =3
+function like(){
+    x++
+    a.innerHTML=x+" like(s)"
+    }
---
+++ Week3/Day 1/Core/script.js
@@ -0,0 +1,19 @@
+var b = document.querySelector(".b")
+var x =9
+function lik(){
+    x++
+    b.innerHTML=x+" like(s)"
+    }
+var g = document.querySelector(".g")
+var x =12
+function li(){
+    x++
+    g.innerHTML=x+" like(s)"
+    }
+var w = document.querySelector(".w")
+var x =9
+function like(){
+    x++
+    w.innerHTML=x+" like(s)"
+    }
+
