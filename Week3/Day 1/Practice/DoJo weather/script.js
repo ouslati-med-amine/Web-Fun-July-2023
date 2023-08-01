e9d2eaa8bc53fb6ead1464fd373029417ff2029c
unknown <medamineouslati85@gmail.com>
2023-08-01 12:25:39 +0200

    DoJo weather

---
+++ Week3/Day 1/Practice/DoJo weather/script.js
@@ -0,0 +1,22 @@
+var a =document.querySelector(".lower")
+function delit(){
+    a.remove()
+}
+var b =document.querySelector(".g")
+var c =document.querySelector(".gg")
+var d =document.querySelector(".ggg")
+var e =document.querySelector(".gggg")
+var f =document.querySelector(".ggggg")
+var g =document.querySelector(".gggggg")
+var h =document.querySelector(".ggggggg")
+var i =document.querySelector(".gggggggg")
+function cover(){
+    b.innerHTML= "75°"
+    c.innerHTML= "65°"
+    d.innerHTML= "80°"
+    e.innerHTML= "66°"
+    f.innerHTML= "69°"
+    g.innerHTML= "61°"
+    h.innerHTML= "78°"
+    i.innerHTML= "70°"
+}
